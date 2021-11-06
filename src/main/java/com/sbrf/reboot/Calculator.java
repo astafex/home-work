0d4a795f16a6de3daaaac0cba60ab8ef48df0518
Artem Astafev <astafev.ph@gmail.com>
2021-11-06 11:08:05 +0300

    add new class - Calculator

---
+++ src/main/java/com/sbrf/reboot/Calculator.java
@@ -0,0 +1,19 @@
+package com.sbrf.reboot;
+
+public class Calculator {
+    public static double getAddition(double a, double b) {
+        return a + b;
+    }
+
+    public static double getSubtraction(double a, double b) {
+        return a - b;
+    }
+
+    public static double getMultiplication(double a, double b) {
+        return a * b;
+    }
+
+    public static double getDivision(double a, double b) {
+        return a / b;
+    }
+}
