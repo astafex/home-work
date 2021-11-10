b3a01bb4391963f9a948fc598592a6d87d801494
Artem Astafev <astafev.ph@gmail.com>
2021-11-11 00:43:26 +0300

    lesson3. add interface AccountRepository, add class AccountService add class Account

---
+++ src/main/java/com/sbrf/reboot/Account.java
@@ -0,0 +1,10 @@
+package com.sbrf.reboot;
+
+ public class Account {
+    private long clientId;
+    private String book;
+
+    public Account(String book) {
+        this.book = book;
+    }
+}
---
+++ src/main/java/com/sbrf/reboot/AccountService.java
@@ -0,0 +1,15 @@
+package com.sbrf.reboot;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Set;
+
+@AllArgsConstructor
+public class AccountService {
+    private AccountRepository accountRepository;
+
+    public boolean isAccountExist(long clientId, Account account) {
+        Set<Account> accounts = accountRepository.getAllAccountsByClientId(clientId);
+        return accounts.contains(account);
+    }
+}
